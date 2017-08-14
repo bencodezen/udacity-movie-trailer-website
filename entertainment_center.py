0740e08014c80bdcca3f2279073fc0c4c1651ef3
Ben Hong <bencodezen@gmail.com>
2017-08-14 10:24:47 -0400

    style: Add noqa to remove URLs from style check

--- entertainment_center.py
+++ entertainment_center.py
@@ -7,16 +7,17 @@ import movie
 
 # Create four instances of Movie class
 the_incredibles = movie.Movie("The Incredibles",
-                              "https://upload.wikimedia.org/wikipedia/en/e/ec/The_Incredibles.jpg",
+                              "https://upload.wikimedia.org/wikipedia/en/e/ec/The_Incredibles.jpg",  # noqa
                               "https://www.youtube.com/watch?v=eZbzbC9285I")
 ratatouille = movie.Movie("Ratatouille",
                           "http://bit.ly/2wX8CfR",
+
                           "https://www.youtube.com/watch?v=c3sBBRxDAqk")
 inside_out = movie.Movie("Inside Out",
                          "http://bit.ly/2wWWBqD",
                          "https://www.youtube.com/watch?v=_MC3XuMvsDI")
 monsters_inc = movie.Movie("Monsters Inc",
-                           "https://lumiere-a.akamaihd.net/v1/images/image_3c4add40.jpeg",
+                           "https://lumiere-a.akamaihd.net/v1/images/image_3c4add40.jpeg",  # noqa
                            "https://www.youtube.com/watch?v=cvOQeozL4S0")
 
 # Create a list of all movies to use in the web page
