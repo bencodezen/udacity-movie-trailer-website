d392a647148f1508e0fae6c91d8d875e5a06de67
Ben Hong <bencodezen@gmail.com>
2017-08-14 08:54:16 -0400

    docs: Add Docstring to Movie class

--- movie.py
+++ movie.py
@@ -2,7 +2,17 @@
 Define the Movie class
 """
 class Movie():
+    """
+    Movie class object to store movie related information
+    """
     def __init__(self, movie_title, movie_artwork, movie_trailer_url):
+        """
+        Initialize an instance of the Movie class
+
+        :param movie_title: string
+        :param movie_artwork: string
+        :param movie_trailer_url: string
+        """
         self.title = movie_title
         self.poster_image_url = movie_artwork
         self.trailer_youtube_url = movie_trailer_url
