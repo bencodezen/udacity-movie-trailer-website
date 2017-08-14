55bcd36e04060c7644803059b2e630f531f7abd8
Ben Hong <bencodezen@gmail.com>
2017-08-14 08:48:26 -0400

    style: Fix code styles to be PEP8 compliant

--- movie.py
+++ movie.py
@@ -1,5 +1,5 @@
 class Movie():
-	def __init__(self, movie_title, movie_artwork, movie_trailer_url):
-		self.title = movie_title
-		self.poster_image_url = movie_artwork
-		self.trailer_youtube_url = movie_trailer_url
+    def __init__(self, movie_title, movie_artwork, movie_trailer_url):
+        self.title = movie_title
+        self.poster_image_url = movie_artwork
+        self.trailer_youtube_url = movie_trailer_url
